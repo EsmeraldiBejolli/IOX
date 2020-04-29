58beae878f144ed9401d5d2b8bb4d8e552132f48
ebejolli <esmeraldi.bejolli@itslearning.com>
2020-04-29 10:37:31 +0200

    initial commit

---
+++ IOX/IOX/IOX.h
@@ -0,0 +1,19 @@
+//
+//  IOX.h
+//  IOX
+//
+//  Created by Esmeraldi Bejolli on 28/04/2020.
+//  Copyright © 2020 ebejolli. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOX.
+FOUNDATION_EXPORT double IOXVersionNumber;
+
+//! Project version string for IOX.
+FOUNDATION_EXPORT const unsigned char IOXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOX/PublicHeader.h>
+
+
